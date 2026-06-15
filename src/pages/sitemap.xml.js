7757092613daa9ae1419728f268445726848a0e4
Nguyễn Long <longnguyenreview@gmail.com>
2026-06-15 08:43:36 +0700

    seo: thêm lastmod cho trang tĩnh trong sitemap (trang chủ 15/6) -> báo Google crawl lại sau khi sửa em-dash

--- src/pages/sitemap.xml.js
+++ src/pages/sitemap.xml.js
@@ -3,17 +3,17 @@ import { getAllArticles, SITE } from '../lib/articles.mjs';
 export async function GET() {
   const arts = await getAllArticles();
   const staticPages = [
-    { loc: `${SITE}/`, cf: 'weekly', pri: '1.0' },
-    { loc: `${SITE}/blog.html`, cf: 'weekly', pri: '0.9' },
-    { loc: `${SITE}/su-kien/`, cf: 'weekly', pri: '0.7' },
-    { loc: `${SITE}/su-kien/wwdc-2026.html`, cf: 'weekly', pri: '0.8' },
-    { loc: `${SITE}/su-kien/samsung-galaxy-unpacked-2026.html`, cf: 'daily', pri: '0.8' },
-    { loc: `${SITE}/cv.html`, cf: 'monthly', pri: '0.5' },
+    { loc: `${SITE}/`, cf: 'weekly', pri: '1.0', lm: '2026-06-15' },
+    { loc: `${SITE}/blog.html`, cf: 'weekly', pri: '0.9', lm: '2026-06-15' },
+    { loc: `${SITE}/su-kien/`, cf: 'weekly', pri: '0.7', lm: '2026-06-15' },
+    { loc: `${SITE}/su-kien/wwdc-2026.html`, cf: 'weekly', pri: '0.8', lm: '2026-06-12' },
+    { loc: `${SITE}/su-kien/samsung-galaxy-unpacked-2026.html`, cf: 'daily', pri: '0.8', lm: '2026-06-15' },
+    { loc: `${SITE}/cv.html`, cf: 'monthly', pri: '0.5', lm: '2026-06-15' },
     { loc: `${SITE}/chinh-sach.html`, cf: 'yearly', pri: '0.4' },
   ];
   const urls = [
     ...staticPages.map(
-      (p) => `  <url>\n    <loc>${p.loc}</loc>\n    <changefreq>${p.cf}</changefreq>\n    <priority>${p.pri}</priority>\n  </url>`
+      (p) => `  <url>\n    <loc>${p.loc}</loc>${p.lm ? `\n    <lastmod>${p.lm}</lastmod>` : ''}\n    <changefreq>${p.cf}</changefreq>\n    <priority>${p.pri}</priority>\n  </url>`
     ),
     ...arts.map((a) => {
       const lastmod = (a.dateModified || a.datePublished || '').slice(0, 10);
